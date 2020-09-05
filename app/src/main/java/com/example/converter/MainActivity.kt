f3bed23dde785b3cf58e65cd9e0b44a6fd8ea555
befartok <ancherkasoff@gmail.com>
2020-09-05 23:41:57 +0400

    add all courses calculate

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.converter
 
+import android.R
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
 
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
 
+    private lateinit var courseType: String
     var curencies = arrayOf("USD", "GBP", "EUR", "RUR")
 
     var spinnerStart: Spinner? = null
@@ -27,10 +23,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.converter.R.layout.activity_main)
 
-       // val toolbar= findViewById<Toolbar>(R.id.toolbar)
-       // setSupportActionBar(toolbar);
+        // val toolbar= findViewById<Toolbar>(R.id.toolbar)
+        // setSupportActionBar(toolbar);
 
 
         //init()
@@ -38,50 +34,115 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         textView_msg = this.textView
 
         spinnerStart = this.startingSpinner
-        spinnerStart!!.setOnItemSelectedListener(this)
+        spinnerStart!!.onItemSelectedListener = this
 
-        // Create an ArrayAdapter using a simple spinner layout and languages array
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, curencies)
+        // Create an ArrayAdapter using a simple spinner layout and curencies array
+        val aa = ArrayAdapter(this, R.layout.simple_spinner_item, curencies)
         // Set layout to use when the list of choices appear
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        aa.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
         // Set Adapter to Spinner
         spinnerStart!!.setAdapter(aa)
 
 
         spinnerFin = this.finishSpinner
-        spinnerFin!!.setOnItemSelectedListener(this)
+        spinnerFin!!.onItemSelectedListener = this
 
-        // Create an ArrayAdapter using a simple spinner layout and languages array
-        val aaFin = ArrayAdapter(this, android.R.layout.simple_spinner_item, curencies)
+        // Create an ArrayAdapter using a simple spinner layout and curencies array
+        val aaFin = ArrayAdapter(this, R.layout.simple_spinner_item, curencies)
         // Set layout to use when the list of choices appear
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        aa.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
         // Set Adapter to Spinner
         spinnerFin!!.adapter = aaFin
 
     }
 
 
-
-    fun init(){
+    fun init() {
 
 
     }
 
+    private var currencyStart: String = ""
+    private var currencyFin: String = ""
     override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
-        textView_msg!!.text = "Selected : "+curencies[position]
+
+        val i: Int? = spinnerStart?.selectedItemPosition
+        val j: Int? = spinnerFin?.selectedItemPosition
+
+        currencyStart = curencies[i!!]
+        currencyFin = curencies[j!!]
+        //textView_msg!!.text = "Selected 1 : " + strName1 + " Selected 2 : " + strName2
+
     }
+
     override fun onNothingSelected(arg0: AdapterView<*>) {
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu,menu)
+/*    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_converter, menu)
+        return true
+    }*/
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // TODO Auto-generated method stub
+        menu.add(getString(com.example.converter.R.string.history))
+        menu.add(getString(com.example.converter.R.string.about))
+        menu.add(getString(com.example.converter.R.string.exit))
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
     }
 
 
     fun runConverting(view: View) {
-        NewThread(this, "USD").execute()
+        var codeOfCurrencyStart = ""
+        var codeOfCurrencyFin = ""
+// TODO: 05.09.2020 упростить 
+        when {
+            currencyStart == currencyFin -> courseType = "self"
+            currencyFin == "RUR" -> {
+                courseType = "direct"
+                when (currencyStart) {
+                    "USD" -> codeOfCurrencyStart = "R01235"
+                    "GBP" -> codeOfCurrencyStart = "R01035"
+                    "EUR" -> codeOfCurrencyStart = "R01239"
+                    "RUR" -> codeOfCurrencyStart = "RU"
+                }
+            }
+            currencyStart == "RUR" -> {
+                courseType = "inverse"
+                when (currencyFin) {
+                    "USD" -> codeOfCurrencyFin = "R01235"
+                    "GBP" -> codeOfCurrencyFin = "R01035"
+                    "EUR" -> codeOfCurrencyFin = "R01239"
+                    "RUR" -> codeOfCurrencyFin = "RU"
+                }
+            }
+            currencyStart != "RUR" && currencyFin != "RUR" -> {
+                courseType = "cross"
+                when (currencyStart) {
+                    "USD" -> codeOfCurrencyStart = "R01235"
+                    "GBP" -> codeOfCurrencyStart = "R01035"
+                    "EUR" -> codeOfCurrencyStart = "R01239"
+                    "RUR" -> codeOfCurrencyStart = "RU"
+                }
+                when (currencyFin) {
+                    "USD" -> codeOfCurrencyFin = "R01235"
+                    "GBP" -> codeOfCurrencyFin = "R01035"
+                    "EUR" -> codeOfCurrencyFin = "R01239"
+                    "RUR" -> codeOfCurrencyFin = "RU"
+                }
+
+            }
+        }
+
+        NewThread(this, codeOfCurrencyStart, codeOfCurrencyFin, courseType).execute()
 
     }
 
